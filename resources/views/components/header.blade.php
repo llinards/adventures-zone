8e4650ca52372632827e6408f517d04fb4fd322c
Linards <linards_lazdins@hotmail.com>
2019-12-14 12:33:20 +0200

    project cleanup

--- resources/views/components/header.blade.php
+++
@@ -1,9 +0,0 @@
-<header>
-  @include('components.navbar')
-  <div class="header-text d-flex flex-column align-items-center justify-content-center">
-      <div class="header-text-container d-flex flex-column">
-        <h1 class="text-uppercase text-light text-weight-bold text-center">Adventures' zone</h1>
-        <h3 class="text-uppercase text-light text-weight-bold text-center align-self-end">Piedzīvojumi, kas attīsta</h3>
-      </div>
-    </div>
-</header>
