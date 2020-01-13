5b5db9c30897de4d0ea13aa79bb9213f13335f0a
Linards <linards_lazdins@hotmail.com>
2020-01-13 13:05:32 +0200

    header in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Adventures' Zone</h1>
+    <header>
+        @include('inc.navbar')
+    </header>
 @endsection
---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav id="desktop-navbar">
+  <div class="az-logo d-flex justify-content-center">
+      <img class="az-logo--img" src="/storage/img/logo_pink.png" alt="az-logo-pink">
+  </div>
+  <div id="left" class="az-menubar d-flex justify-content-center align-items-center">
+      <li class="nav-item">
+        <a class="nav-link text-white text-uppercase" href="#">{{ __('attrakcijas') }}</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white text-uppercase" href="#">ballītes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white text-uppercase" href="#">vērtības</a>
+      </li>
+  </div>
+  <div id="right" class="az-menubar d-flex justify-content-center align-items-center">
+    
+  </div>
+</nav>
