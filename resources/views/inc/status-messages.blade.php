fe093a8929ba9f3e7e810ca8ba6cfbd09e4e95dd
Linards <linards_lazdins@hotmail.com>
2020-04-08 12:46:50 +0300

    visual changes

--- resources/views/inc/status-messages.blade.php
+++ resources/views/inc/status-messages.blade.php
@@ -1,12 +1,12 @@
 @if ($message = Session::get('success'))
-<div class="alert alert-success alert-block">
+<div class="alert alert-success alert-block mt-3">
    <button type="button" class="close" data-dismiss="alert">×</button>	
    <strong>{{ $message }}</strong>
 </div>
 @endif
 
 @if ($message = Session::get('error'))
-<div class="alert alert-danger alert-block">
+<div class="alert alert-danger alert-block mt-3">
    <button type="button" class="close" data-dismiss="alert">×</button>	
    <strong>{{ $message }}</strong>
 </div>
